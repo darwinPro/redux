c2f321a6b73263ac3718f788e38cebbf99b0c068
darwin.da64 <darwin.DA64@gmail.com>
2018-07-16 10:14:39 -0500

    Agregando mi primer componente

--- platzi-vidio/index.js
+++ platzi-vidio/index.js
@@ -1,1 +1,10 @@
-console.log("js");
+import React from 'react';
+import {render} from 'react-dom';
+import Media from '../src/playlist/components/media'
+
+//const root = document.getElementById('root');
+//const holaMundo = <h1>Hola Estudiante</h1>;
+
+
+render( <Media/> ,root);
+//console.log("js");
---
+++ src/playlist/components/media.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Media extends Component {
+    render() {
+        return (
+            <div>
+                <img
+                    src=""
+                    alt=""
+                    width={260}
+                    height={150}
+                />
+                <h3>Por que aprender React</h3>
+                <p>Darwin Alvarez</p>
+            </div>
+
+        );
+    }
+}
+
+export default Media;
